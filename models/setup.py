8174e45f93060ad76177718a10c727675a5d0af3
acruzmtz <alejandrocruzmtz@outlook.com>
2021-12-18 11:16:09 -0600

    the files to connect with the db were created

---
+++ models/setup.py
@@ -0,0 +1,19 @@
+from database import Connection
+
+
+def read_tables(path):
+    with open(path, 'r') as file:
+        return file.read()
+
+
+def create_tables():
+    db = Connection()
+    path = "models/sql/tables.sql"
+    sql = read_tables(path)
+
+    try:
+        db.execute(sql)
+    except Exception as e:
+        return False
+    else:
+        return True
