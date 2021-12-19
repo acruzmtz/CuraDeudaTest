5a28886b2b4864cb34be296e7870d3e74374074e
acruzmtz <alejandrocruzmtz@outlook.com>
2021-12-18 23:40:08 -0600

    fix: resolve error to create tables in database

--- models/setup.py
+++ models/setup.py
@@ -1,18 +1,23 @@
 from models.database import Connection
+from models.sql.tables import STATE, TOWNSHIP, SUBURB, SHIPPING, SET_FOREING_KEYS, SET_INDEX
 
-
-def read_tables(path):
-    with open(path, 'r') as file:
-        return file.read()
+# def read_tables(path):
+#     with open(path, 'r') as file:
+#         return file.read()
 
 
 def create_tables():
     db = Connection()
     path = "models/sql/tables.sql"
-    sql = read_tables(path)
+    # sql = read_tables(path)
 
     try:
-        db.execute(sql)
+        db.execute(STATE)
+        db.execute(TOWNSHIP)
+        db.execute(SUBURB)
+        db.execute(SHIPPING)
+        db.execute(SET_INDEX)
+        db.execute(SET_FOREING_KEYS)
     except Exception as e:
         return False
     else:
--- models/sql/tables.py
+++ models/sql/tables.py
@@ -1,49 +1,55 @@
 
--- # TABLE: estado
+# TABLE: estado
+STATE = """
+    CREATE TABLE IF NOT EXISTS `estado` (
+    `c_estado` INT NOT NULL,
+    `d_estado` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`c_estado`),
+    UNIQUE INDEX `c_estado_UNIQUE` (`c_estado` ASC))
+"""
 
-CREATE TABLE IF NOT EXISTS `estado` (
-`c_estado` INT NOT NULL,
-`d_estado` VARCHAR(45) NOT NULL,
-PRIMARY KEY (`c_estado`),
-UNIQUE INDEX `c_estado_UNIQUE` (`c_estado` ASC));
 
-
--- # TABLE: municipio
-
+# TABLE: municipio
+TOWNSHIP = """
 CREATE TABLE IF NOT EXISTS `municipio` (
 `c_mnpio` INT NOT NULL,
 `d_mnpio` VARCHAR(45) NOT NULL,
 PRIMARY KEY (`c_mnpio`),
-UNIQUE INDEX `c_estado_UNIQUE` (`c_mnpio` ASC));
+UNIQUE INDEX `c_estado_UNIQUE` (`c_mnpio` ASC))
+"""
 
 
--- # TABLE: colonia
-
-CREATE TABLE IF NOT EXISTS `colonia` (
+# TABLE: colonia
+SUBURB = """
+  CREATE TABLE IF NOT EXISTS `colonia` (
   `d_codigo` INT NOT NULL,
   `d_asenta` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`d_codigo`),
-  UNIQUE INDEX `c_estado_UNIQUE` (`d_codigo` ASC));
+  UNIQUE INDEX `c_estado_UNIQUE` (`d_codigo` ASC))
+"""
 
 
--- # TABLE: envios
-
+# TABLE: envios
+SHIPPING = """
 CREATE TABLE IF NOT EXISTS `envio` (
   `envio_id` INT NOT NULL AUTO_INCREMENT,
   `c_estado` INT NOT NULL,
   `c_mnpio` INT NOT NULL,
   `c_colonia` INT NOT NULL,
   PRIMARY KEY (`envio_id`),
-  UNIQUE INDEX `c_estado_UNIQUE` (`envio_id` ASC));
+  UNIQUE INDEX `c_estado_UNIQUE` (`envio_id` ASC))
+"""
 
 
--- # SET FOREING KEYS
-
+# SET FOREING KEYS
+SET_INDEX = """
 ALTER TABLE `envio`
 ADD INDEX `estado_envio_fk_idx` (`c_estado` ASC),
 ADD INDEX `municipio_envio_fk_idx` (`c_mnpio` ASC),
-ADD INDEX `colonia_envio_fk_idx` (`c_colonia` ASC);
-;
+ADD INDEX `colonia_envio_fk_idx` (`c_colonia` ASC)
+"""
+
+SET_FOREING_KEYS = """
 ALTER TABLE `envio`
 ADD CONSTRAINT `estado_envio_fk`
   FOREIGN KEY (`c_estado`)
@@ -60,3 +66,4 @@ ADD CONSTRAINT `colonia_envio_fk`
   REFERENCES `colonia` (`d_codigo`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
+"""
